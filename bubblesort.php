79afec5062e9f949f4f413a002957e1ec0acfc86
irawan-yanyan <irawan.yanyan@yahoo.com>
2022-10-29 08:45:54 +0700

    second commit

--- bubblesort.php
+++ bubblesort.php
@@ -1,9 +1,20 @@
 <?php
 		echo " bubble sort \n";
 		$data = array(5,4,34,3,6,2,8);
-		for($i=0;$i<count($data);$i++){
-			echo $data[$i]."\n";
+		print_r($data);
+		$n = count($data);
+		for($i=0;$i<$n;$i++){
+			//echo $data[$i]."\n";
+			for($j=$n-1;$j>$i;$j--){
+				if($data[$j]< $data[$j-1]){
+					$dummy = $data[$j];
+					$data[$j] = $data[$j-1];
+					$data[$j-1] = $dummy;
+				}
+
+			}
 
 		}
-
+	
+		print_r($data);	
 ?>
